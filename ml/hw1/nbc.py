bdd96640fcd675e68fd9f1a76f46b4dd768291d3
Primal Pappachan <primal007@gmail.com>
2013-02-20 03:39:49 -0500

    accuracy improved to 94.5%

--- ml/hw1/nbc.py
+++ ml/hw1/nbc.py
@@ -1,7 +1,8 @@
 import pdb
+import random
 
 class AutoVivification(dict):
-    """Implementation of perl's autovivification feature."""
+	#Ref: http://stackoverflow.com/questions/651794/whats-the-best-way-to-initialize-a-dict-of-dicts-in-python
     def __getitem__(self, item):
         try:
             return dict.__getitem__(self, item)
@@ -9,21 +10,6 @@ class AutoVivification(dict):
             value = self[item] = type(self)()
             return value
 
-def train(sample):
-	"""Increment category no in a list whose len = no of categories
-		Increment attr value w.r.t category no, cat[attr1] = [v1, v2, ........., vn]
-	"""
-	pass
-
-
-
-def classify(sample, default):	
-
-	#for each class label: compute p(yk)
-	#	for each attribute: compute p(ai) and the product
-	#	multiply and store in the list [c1, c2, ............., cn]
-	#return index of maximum value in the list	
-	pass
 
 def add_category(categories, category):
 	if categories.has_key(category):
@@ -37,29 +23,26 @@ def add_attribute(a, category, attr, value):
 	else: a[category][attr][value] = 1
 	return a
 
-def cond_probability(a, category, attr, value):
-	s = 0
+def cond_probability(a, category, attr, value, categories):
 	if a[category][attr].has_key(value):
-		x = a[category][attr][value]
+		x = a[category][attr][value] + 1
 	else: x = 1	
-	for key in a[category][attr].keys():
-		s = s + a[category][attr][key]
-	return x/float(s)	
+	return x/float(categories[category]+sum(categories.values()))	
 
 def main(filename):
 	instances = open(filename).read().splitlines()
-	#Split of 2/3 and 1/3
-	split = int((2/3.0) * len(instances))
+	random.shuffle(instances) #Randomizes the instances so as to improve distribution
+	split = int((2/3.0) * len(instances)) #Split of 2/3 and 1/3
 	trainset = instances[:split]
 	testset = instances[split:]	
 	categories = dict()
 	a = AutoVivification() #data-store for [category][attribute][value] counts
 	flag = True #flag to check if attribute has been added once
+	
 	for t in trainset:
 		sample = t.split(',')		
-		category = sample[0]
+		category, attributes = sample[0], sample[1:] #First element of instance = category, Rest = Attributes
 		categories = add_category(categories, category)
-		attributes = sample[1:]
 		for i in range(len(attributes)):
 			value = attributes[i]
 			if flag:
@@ -68,29 +51,31 @@ def main(filename):
 				a = add_attribute(a, category, i, value)				
 		flag = False
 
-	#Estimate probability for a class label
-	for k in categories.keys():
-		prob_k = categories[k]/float(split)
-		categories[k] = prob_k
-
 #	print categories, a
-
-	max = 0.0
+	p = e = acc = 0
 	for t in testset:
 		sample = t.split(',')
-		print sample	
-		label = sample[0]
-		attributes = sample[1:]
+	#	print sample	
+		label, attributes = sample[0], sample[1:]
+		max = 0.0
 		for k in categories.keys():
 			product = 1
 			for i in range(len(attributes)):
 				value = attributes[i]
-				product *= cond_probability(a, k, i, value) 
-			if product * categories[k] > max: 
-				max = product * categories[k]
+				product *= cond_probability(a, k, i, value, categories) 
+	#		print product * categories[k], k
+	#		print "max", max
+			arg = product * (categories[k]/float(split))
+			if arg > max: 
+				max = arg
 				predicted = k
-		print predicted	
-		 		
+		#	print "max", max	
+		#print "predicted", predicted		
+		if predicted == 'e': e+=1 
+		else: p+=1						
+		if predicted == label: acc+= 1	
+	print e, p 	 		
+	print acc/float(len(testset))
 
 if __name__ == "__main__":
 	main('testfile.txt')
